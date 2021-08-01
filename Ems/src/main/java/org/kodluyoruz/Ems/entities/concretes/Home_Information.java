073c965446b10b124e8896026582abc4001663bc
Muammer Agtas <programuammer@gmail.com>
2021-08-01 21:20:59 +0300

    Add files via upload

---
+++ Ems/src/main/java/org/kodluyoruz/Ems/entities/concretes/Home_Information.java
@@ -0,0 +1,54 @@
+package org.kodluyoruz.Ems.entities.concretes;
+
+import java.awt.image.BufferedImage;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "home_information")
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties (value = { "hibernateLazyInitializer", "handler"})
+public class Home_Information {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name= "id")
+	@PrimaryKeyJoinColumn
+	private int homeInformationId;
+	
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "year")
+	private int year;
+	
+	@Column(name = "balcony")
+	private boolean balcony;
+	
+	@Column(name = "number_of_rooms")
+	private int numberOfRooms;
+	
+	@Column(name = "price")
+	private double price;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "image")
+	private BufferedImage image;
+
+}
